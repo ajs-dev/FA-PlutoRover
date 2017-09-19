158c18fe7a83b3687c4dac9c03f47727750c80ec
ajs-dev <dev@gnosistem.com>
2017-09-20 00:31:17 +0100

    Fulfil left and right tests

--- src/FA.PlutoRover.Api/PlutoRover.cs
+++ src/FA.PlutoRover.Api/PlutoRover.cs
@@ -39,11 +39,66 @@ namespace FA.PlutoRover.Api
                 case Movement.Backwards:
                     newLocation = new Location(CurrentLocation.X, CurrentLocation.Y - 1, CurrentLocation.Orientation);
                     break;
+                case Movement.TurnRight:
+                    var newOrientation = ChangeOrientationRight(CurrentLocation.Orientation);
+                    newLocation = new Location(CurrentLocation.X, CurrentLocation.Y, ChangeOrientationRight(CurrentLocation.Orientation));
+                    break;
+                case Movement.TurnLeft:
+                    newLocation = new Location(CurrentLocation.X, CurrentLocation.Y, ChangeOrientationLeft(CurrentLocation.Orientation));
+                    break;
                 default:
                     throw new InvalidOperationException($"The specified journey \"{journey}\" for the rover is invalid");
             }
 
             return newLocation;
+        }
+
+        private OrientationEnum ChangeOrientationRight(OrientationEnum orientation)
+        {
+            OrientationEnum newOr;
+            switch (orientation)
+            {
+                case OrientationEnum.North:
+                    newOr = OrientationEnum.East;
+                    break;
+                case OrientationEnum.East:
+                    newOr = OrientationEnum.South;
+                    break;
+                case OrientationEnum.South:
+                    newOr = OrientationEnum.West;
+                    break;
+                case OrientationEnum.West:
+                    newOr = OrientationEnum.North;
+                    break;
+                default:
+                    newOr = OrientationEnum.North;
+                    break;
+            }
+            return newOr;
+        }
+
+        private OrientationEnum ChangeOrientationLeft(OrientationEnum orientation)
+        {
+            OrientationEnum newOr;
+            switch (orientation)
+            {
+                case OrientationEnum.North:
+                    newOr = OrientationEnum.West;
+                    break;
+                case OrientationEnum.West:
+                    newOr = OrientationEnum.South;
+                    break;
+                case OrientationEnum.South:
+                    newOr = OrientationEnum.East;
+                    break;
+                case OrientationEnum.East:
+                    newOr = OrientationEnum.North;
+                    break;
+                default:
+                    newOr = OrientationEnum.North;
+                    break;
+            }
+            return newOr;
         }
     }
 }
