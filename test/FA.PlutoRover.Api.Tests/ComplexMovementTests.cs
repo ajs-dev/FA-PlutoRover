850af75f6f6b26c4f0ae41515ae8a82ca17ec6d0
ajs-dev <dev@gnosistem.com>
2017-09-21 21:56:56 +0100

    Added requirements example as it can now be fulfilled by api logic.

--- test/FA.PlutoRover.Api.Tests/ComplexMovementTests.cs
+++ test/FA.PlutoRover.Api.Tests/ComplexMovementTests.cs
@@ -28,5 +28,26 @@ namespace FA.PlutoRover.Api.Tests
             Assert.That(newLocation.Orientation, Is.EqualTo(expectedLocation.Orientation));
         }
 
+        [Test]
+        public void MoveFFRFF_RequirementsExample()
+        {
+            //Arrange
+            var start = new Point();
+            var grid = new Point(100, 100);
+            var orientation = OrientationEnum.North;
+            IMovement rover = new PlutoRover(grid, start, orientation);
+
+            const string moveCommand = "FFRFF";
+            ILocation expectedLocation = new Location(2, 2, OrientationEnum.East);
+
+            //Act
+            ILocation newLocation = rover.Move(moveCommand);
+
+            //Assert
+            Assert.That(newLocation, Is.Not.Null);
+            Assert.That(newLocation.X, Is.EqualTo(expectedLocation.X));
+            Assert.That(newLocation.Y, Is.EqualTo(expectedLocation.Y));
+            Assert.That(newLocation.Orientation, Is.EqualTo(expectedLocation.Orientation));
+        }
     }
 }
