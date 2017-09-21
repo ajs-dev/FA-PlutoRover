7ae19c988d83e69f8000661eb83cc86b9ad38ea4
ajs-dev <dev@gnosistem.com>
2017-09-21 22:00:26 +0100

    Random test pattern including grid wrapping at the y axis.

--- test/FA.PlutoRover.Api.Tests/ComplexMovementTests.cs
+++ test/FA.PlutoRover.Api.Tests/ComplexMovementTests.cs
@@ -29,6 +29,28 @@ namespace FA.PlutoRover.Api.Tests
         }
 
         [Test]
+        public void MoveFBBRRFLF_OrientatedNorth()
+        {
+            //Arrange
+            var start = new Point();
+            var grid = new Point(10, 10);
+            var orientation = OrientationEnum.North;
+            IMovement rover = new PlutoRover(grid, start, orientation);
+
+            const string moveCommand = "FBBRRFLF";
+            ILocation expectedLocation = new Location(1, 9, OrientationEnum.East);
+
+            //Act
+            ILocation newLocation = rover.Move(moveCommand);
+
+            //Assert
+            Assert.That(newLocation, Is.Not.Null);
+            Assert.That(newLocation.X, Is.EqualTo(expectedLocation.X));
+            Assert.That(newLocation.Y, Is.EqualTo(expectedLocation.Y));
+            Assert.That(newLocation.Orientation, Is.EqualTo(expectedLocation.Orientation));
+        }
+
+        [Test]
         public void MoveFFRFF_RequirementsExample()
         {
             //Arrange
